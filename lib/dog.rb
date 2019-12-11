2fdc8ff9e4c7f2927fc26dbdec8fa493e0f3eec4
Luis Herrera <herrera.lrh@gmail.com>
2019-12-11 22:20:03 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,5 +1,4 @@
 class Dog
-
   def name=(name)
     @name = name
   end
@@ -15,5 +14,5 @@ class Dog
   def breed
     @breed
   end
-  
+
 end
--- lib/person.rb
+++ lib/person.rb
@@ -1,5 +1,4 @@
 class Person
-
   def name=(name)
     @name = name
   end
@@ -15,4 +14,5 @@ class Person
   def job
     @job
   end
+
 end
